61a0e3a5a7b0f703640e5d0e606cd4324db0a1be
kahenyamercy <kahenyamercy5@gmail.com>
2025-01-02 17:42:44 +0300

    project setup

---
+++ hivemind/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to HiveMind 🐝</h1>
+      <p>Start exploring your community!</p>
+    </div>
+  );
+}
+
+export default HomePage;
